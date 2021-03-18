ea853557774389de3c4d5c0f1cde0664876b81e8
Ethan Liu <ethan@EthandeMacBook-Pro.local>
2021-03-18 09:37:21 +0800

    voice test

---
+++ voice.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+from aip import AipSpeech
+
+'''your APPID AK SK'''
+APP_ID = '23814855'
+API_KEY = 'saYGlZIVyX6hgIOv92B6fCAo'
+SECRET_KEY = 'ZbSdVMdmohIdAK5pf4yrip1h0kffHayf'
+
+client = AipSpeech(APP_ID,API_KEY,SECRET_KEY)
+
+s='你好，刘伊墨，我是你的语音助手，有什么困难可以来联系我'
+
+speech = client.synthesis(s, 'zh',1)
+
+if not isinstance(speech, dict):
+    with open('myaudio.mp3','wb') as f:
+        f.write(speech)
